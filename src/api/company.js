27d9e189f5bf4639581c7cb4c0026824e41fa3b0
on <olavnybo@gmail.com>
2016-11-09 18:55:55 +0100

    Removed logging

--- src/api/company.js
+++ src/api/company.js
@@ -3,8 +3,6 @@ const companyUrl = 'http://localhost:3010/company';
 export const getCompany = (companyId) => (
     fetch(`${companyUrl}/${companyId}`)
         .then(function(response) {
-            console.log("response is: ");
-            console.dir(response);
             return response.json()
         })
     );
